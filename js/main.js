04b50ca4ff6546c4e91e118c0492934d1f92eee8
Sherma'n Wilson <sherman.a.wilson@gmail.com>
2021-08-05 01:06:55 -0500

    Java Update

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
 `use strict`
+
+
+$(document).ready(function (){
+
+
+    $(btn).click(function (){
+        $(this).click( function () {
+            alert("Clicked on the button");
+        })
+    })
+
+
+
+
+})
